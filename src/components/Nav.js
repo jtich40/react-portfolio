98b8fe3c9188292338dde30732ec01d1eef2f41a
Jared Tichacek <jared.tichacek@gmail.com>
2023-05-02 18:15:14 -0500

    added click handler to nav component and fixed path for project images

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-function Nav() {
+function Nav({ setPage }) {
+    const handleClick = (event, pageName) => {
+        event.preventDefault();
+        setPage(pageName);
+      };
     return (
         <nav className="navbar navbar-expand-lg fixed-top navbarScroll fade">
         <div className="container">
@@ -12,16 +16,16 @@ function Nav() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto rounded-4 bg-secondary-subtle">
                     <li className="nav-item">
-                        <a className="nav-link rounded-4" id="tab-1" href="#">Home</a>
+                        <a className="nav-link rounded-4" id="tab-1" href="#" onClick={(event) => handleClick(event, 'Home')}>Home</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link rounded-4" id="tab-2" href="#about">About</a>
+                        <a className="nav-link rounded-4" id="tab-2" href="#about" onClick={(event) => handleClick(event, 'About')}>About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link rounded-4" id="tab-3" href="#skills">Skills</a>
+                        <a className="nav-link rounded-4" id="tab-3" href="#skills" onClick={(event) => handleClick(event, 'Skills')}>Skills</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link rounded-4" id="tab-4" href="#projects">Projects</a>
+                        <a className="nav-link rounded-4" id="tab-4" href="#projects" onClick={(event) => handleClick(event, 'Projects')}>Projects</a>
                     </li>
                 </ul>
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,42 +7,42 @@ function Projects() {
     {
         id: 0,
         description: "A blog site to coordinate food donations.",
-        image: '../assets/images/shared-lunch.gif',
+        image: '../images/shared-lunch.gif',
         deployed: 'https://shared-lunch.herokuapp.com/',
         github: 'https://github.com/jtich40/shared-lunch'
     },
     {
         id: 1,
         description: "A search engine that finds GIFs for your favorite movies.",
-        image: '../assets/images/miphy.gif',
+        image: '../images/miphy.gif',
         deployed: 'https://jtich40.github.io/miphy/',
         github: 'https://github.com/jtich40/miphy'
     },
     {
         id: 2,
         description: "A simple command-line application to manage employee information.",
-        image: '../assets/images/coding-quiz.gif',
+        image: '../images/coding-quiz.gif',
         deployed: 'https://shared-lunch.herokuapp.com/',
         github: 'https://github.com/jtich40/employee-database'
     },
     {
         id: 3,
         description: "A weather app that provides the current and 5-day forecast.",
-        image: '../assets/images/weather-dashboard.gif',
+        image: '../images/weather-dashboard.gif',
         deployed: 'https://jtich40.github.io/weather-dashboard/',
         github: 'https://github.com/jtich40/weather-dashboard'
     },
     {
         id: 4,
         description: "A timed coding quiz with multiple choice questions.",
-        image: '../assets/images/coding-quiz.gif',
+        image: '../images/coding-quiz.gif',
         deployed: 'https://jtich40.github.io/coding-quiz/',
         github: 'https://github.com/jtich40/shared-lunch'
     },
     {
         id: 5,
         description: "A backend social network application.",
-        image: '../assets/images/flame-chat.gif',
+        image: '../images/flame-chat.gif',
         deployed: 'https://github.com/jtich40/social-network',
         github: 'https://github.com/jtich40/social-network'
         
