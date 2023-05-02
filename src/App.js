a9974274fc37624347de35e994ae453085e7b312
Jared Tichacek <jared.tichacek@gmail.com>
2023-05-02 14:33:14 -0500

    added styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './components/Home';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import './App.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   const [page, setPage] = useState('Home')
